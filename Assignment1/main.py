015e79a9880f8946c2e0249e382330b65abbfcb5
Anubhava Tripathi <anubhava_t@outlook.com>
2026-07-15 19:51:53 +0530

    Stage 2: read endponits with 404

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+tasks=[
+    {"id":1,"title":"Watch lecture","done":False},
+    {"id":2,"title":"Do assignment","done":True},
+    {"id":3,"title":"Wash dishes","done":True}
+]
+
 
 app=FastAPI();
 
@@ -9,3 +15,16 @@ def read_root():
 @app.get("/health")
 def get_health():
     return {"status":"ok"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task_by_id(id:int):
+    task=[t for t in tasks if t["id"]==id]
+    if task:
+        return task[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"Task {id} not found")
+        
