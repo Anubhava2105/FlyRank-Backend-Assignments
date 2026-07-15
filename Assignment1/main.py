ef6b8b0a76b4dbf926cf575454b0c4fa12149bb1
Anubhava Tripathi <anubhava_t@outlook.com>
2026-07-15 20:34:50 +0530

    stage 4: full CRUD

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 tasks=[
     {"id":1,"title":"Watch lecture","done":False},
     {"id":2,"title":"Do assignment","done":True},
@@ -7,6 +8,11 @@ tasks=[
 
 
 app=FastAPI();
+
+class TaskModel(BaseModel):
+    id: int = None
+    title: str
+    done: bool = False
 
 @app.get("/")
 def read_root():
@@ -28,17 +34,26 @@ def get_task_by_id(id:int):
     else:
         raise HTTPException(status_code=404, detail=f"Task {id} not found")
         
-@app.post("/tasks")
-
-def create_task(title:str):
-    if(title is None):
-        raise HTTPException(status_code=400,detail="Title is required")
-    max=0
-    for t in tasks:
-        if t["id"]>max:
-            max=t["id"]
-    new_task={"id":max+1,"title":title,"done":False}
+@app.post("/tasks", status_code=201)
+def create_task(task:TaskModel):
+    new_task = {"id": len(tasks) + 1, "title": task.title, "done": task.done}
     tasks.append(new_task)
-    raise HTTPException(status_code=201,detail="Task created successfully") 
     return new_task
 
+@app.put("/tasks/{id}", status_code=200)
+def update_tasks(id: int, task: TaskModel):
+    found_task = [t for t in tasks if t["id"]==id]
+    if(not found_task):
+        raise HTTPException(status_code=404,detail=f"Task {id} not found")
+    if(task.title is not None):
+        found_task[0]["title"]=task.title
+    if(task.done is not None):
+        found_task[0]["done"]=task.done
+    return found_task[0]
+
+@app.delete("/tasks/{id}", status_code=204)
+def delete_task(id: int):
+    found_task = [t for t in tasks if t["id"]==id]
+    if(not found_task):
+        raise HTTPException(status_code=404,detail=f"Task {id} not found")
+    tasks.remove(found_task[0])
